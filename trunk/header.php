5c80b10588af8d6600e3d57216a93e1e5b826dfe
artesvisualescom <artesvisualescom@93633818-fc7b-1754-aca9-755e533ee963>
2011-01-28 16:51:31 +0000

    Autoversioning commit:  a non-deltaV client made a change to /trunk/header.php

---
+++ trunk/header.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>
+
+<head profile="http://gmpg.org/xfn/11">
+    <title><?php
+        if ( is_single() ) { single_post_title(); }      
+        elseif ( is_home() || is_front_page() ) { bloginfo('name'); print ' | '; bloginfo('description'); get_page_number(); }
+        elseif ( is_page() ) { single_post_title(''); }
+        elseif ( is_search() ) { bloginfo('name'); print ' | Search results for ' . wp_specialchars($s); get_page_number(); }
+        elseif ( is_404() ) { bloginfo('name'); print ' | Not Found'; }
+        else { bloginfo('name'); wp_title('|'); get_page_number(); }
+    ?></title>
+ 
+ <meta http-equiv="content-type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+ 
+ <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
+ 
+ <?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
+ 
+ <?php wp_head(); ?>
+ 
+ <link rel="alternate" type="application/rss+xml" href="<?php bloginfo('rss2_url'); ?>" title="<?php printf( __( '%s latest posts', 'your-theme' ), wp_specialchars( get_bloginfo('name'), 1 ) ); ?>" />
+ <link rel="alternate" type="application/rss+xml" href="<?php bloginfo('comments_rss2_url') ?>" title="<?php printf( __( '%s latest comments', 'your-theme' ), wp_specialchars( get_bloginfo('name'), 1 ) ); ?>" />
+ <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+ 
+ <script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-11642988-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+</head>
+
+<body>
+
+<div id="pagewidth" class="hfeed">
+
+	<div id="header">
+	<div id="branding">
+		<div id="blog-title"><span><a href="<?php bloginfo( 'url' ) ?>/" title="<?php bloginfo( 'name' ) ?>" rel="home"><?php bloginfo( 'name' ) ?></a></span></div>
+		<?php if ( is_home() || is_front_page() ) { ?>
+		    <h1 id="blog-description"><?php bloginfo( 'description' ) ?></h1>
+		<?php } else { ?>
+		    <div id="blog-description"><?php bloginfo( 'description' ) ?></div>
+		<?php } ?>
+	</div><!– #branding –>
+      
+    <div id="access">
+            <div class="skip-link"><a href="#content" title="<?php _e( 'Skip to content', 'your-theme' ) ?>"><?php _e( 'Skip to content', 'your-theme' ) ?></a></div>
+            <?php wp_page_menu( 'sort_column=menu_order' ); ?>                      
+    </div><!-- #access -->
+   
+  </div><!– #header –>
+  
+  <div id="wrapper" class="clearfix">
