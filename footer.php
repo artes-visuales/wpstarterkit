5c71db32a068e2edae6e715b199faea3a7913515
artesvisualescom <artesvisualescom@93633818-fc7b-1754-aca9-755e533ee963>
2011-01-28 16:51:30 +0000

    Autoversioning commit:  a non-deltaV client made a change to /trunk/footer.php

---
+++ footer.php
@@ -0,0 +1,19 @@
+</div><!-- #wrapper -->
+
+<div id="footer">
+	
+	<p class="small">
+	Powered by <a href="http://wordpress.org/" title="WordPress">WordPress</a>  diseñado por <a href="http://fbtemplate.wordpress.com/" title="Example Web Design">fbtemplate</a>
+	</p>
+	<p><a href="feed:<?php bloginfo('rss2_url'); ?>"><img src="http://icons.iconarchive.com/icons/fasticon/web-2/16/Feed-icon.png" alt="RSS Feed Icon" />Posts RSS</a>
+| <a href="feed:<?php bloginfo('comments_rss2_url'); ?>"><img src="http://icons.iconarchive.com/icons/fasticon/web-2/16/Feed-icon.png" alt="RSS Feed Icon" /> Comentarios RSS</a>
+	</p>
+        
+</div><!-- #footer -->
+        
+</div><!-- #pagewidth --> 
+
+</body>
+</html>
+
+
